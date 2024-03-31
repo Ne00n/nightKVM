675b06d3649b72aee7173c8f86aa07570972e43d
Neoon <neo@tiefkuehler-madness.me>
2024-03-31 21:20:18 +0200

    better error handling

--- agent.py
+++ agent.py
@@ -5,14 +5,22 @@ print("Loading config")
 with open(f"agent.json") as f: config = json.load(f)
 
 async def run(remote="127.0.0.1:3000"):
-    uri = f"ws://{remote}"
-    print(f"Connecting to {uri}")
-    async with websockets.connect(uri) as websocket:
-        print(f"Connected")
-        while True:
-            response = await websocket.recv()
-            print(response)
-            time.sleep(1)
+    while True:
+        uri = f"ws://{remote}"
+        print(f"Connecting to {uri}")
+        try:
+            async with websockets.connect(uri) as websocket:
+                print(f"Connected")
+                await websocket.send(f"token:{config['name']}:{config['token']}")
+                while True:
+                    await websocket.send("jobs")
+                    response = await websocket.recv()
+                    print(response)
+                    time.sleep(10)
+        except Exception as ex:
+            print("Disconnected")
+            print(ex)
+            time.sleep(20)
 
 if __name__ == "__main__":
     print("Ready")
